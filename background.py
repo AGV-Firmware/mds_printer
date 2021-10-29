f303ab4ac688c685701f7d52aa76392623c2cacf
Shubham Chodankar <80900453+AGV-Firmware@users.noreply.github.com>
2021-10-29 14:22:36 +0530

    Update background.py

--- background.py
+++ background.py
@@ -20,7 +20,7 @@ tsclibrary.about();
 '''
 tsclibrary.openportW("COM3");
 '''
-tsclibrary.openportW("USB");
+tsclibrary.openportW("/dev/ttyUSB0");
 '''
 #// AUTODETECT
 tsclibrary.sendcommandW("SIZE 100 mm, 50 mm"); #// size of label width mm, height mm - 100 x 50
@@ -28,6 +28,7 @@ tsclibrary.sendcommandW("GAP 3 mm, 0 mm");
 tsclibrary.sendcommandW("DIRECTION 1");
 tsclibrary.sendcommandW("CLS");
 tsclibrary.sendcommandW("TEXT 10,30,\"3\",0,1,1,\"123456\"");
+
 #tsclibrary.barcodeW("10","150","EAN13","80","1","0","2","4","123456789012");
 #tsclibrary.printerfontW("10", "70", "4", "0", "1", "1", "TEST PRINTOUT");
 #tsclibrary.windowsfontW("10","100","24","0", "0", "0", "Arial","Window Font Test");
